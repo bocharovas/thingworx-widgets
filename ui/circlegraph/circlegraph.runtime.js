3187b7464a86a07f3f4495302ae2e28235675f2f
bocharov <ecoline-bocharov@bk.ru>
2022-02-01 10:02:00 +0300

    gitignore

--- ui/circlegraph/circlegraph.runtime.js
+++ ui/circlegraph/circlegraph.runtime.js
@@ -5,14 +5,14 @@
 
 	TW.Runtime.Widgets.circlegraph = function () {
 
-		function CreateCircle(canvasName, Radius_Mill){
+		function CreateCircle(canvasName, Radius_Mill) {
 			canvasName.circle()
 				.radius(Radius_Mill)
 				.center(0, 0)
 				.fill('none')
-				.addClass('mbCircle')	
+				.addClass('mbCircle')
 		}
-		
+
 		let canvas;
 		let width, height, Rad_Mill;
 		let vibroData1 = new Array();
@@ -42,17 +42,71 @@
 
 			let trajectoryLine = new SVG.PathArray();
 			for (let j = 0; j < 12; (j = j + 2)) {
-				trajectoryLine[j] = ['M', Rad_Mill * Math.cos((j/2 * 30)  * Math.PI/180), 
-					Rad_Mill * Math.sin((j/2 * 30) * Math.PI/180)];				
-				trajectoryLine[j+1] = ['L', Rad_Mill * Math.cos(((j/2 * 30) + 180) * Math.PI/180),
-					Rad_Mill * Math.sin(((j/2 * 30) + 180) * Math.PI/180)];
-			}	
+				let x = Rad_Mill * Math.cos((j / 2 * 30) * Math.PI / 180);
+				let y = Rad_Mill * Math.sin((j / 2 * 30) * Math.PI / 180);
+				let xn = Rad_Mill * Math.cos(((j / 2 * 30) + 180) * Math.PI / 180);
+				let yn = Rad_Mill * Math.sin(((j / 2 * 30) + 180) * Math.PI / 180);
+				trajectoryLine[j] = ['M', x, y];
+				trajectoryLine[j + 1] = ['L', xn, yn];
+
+				switch (j) {
+					case 0:
+						x = x + 2
+						y = y + 1
+						xn = xn - 5
+						yn = yn + 1
+						break;
+					case 2:
+						x = x + 2
+						y = y + 3
+						xn = xn - 6
+						yn = yn - 1
+						break;
+					case 4:
+						x = x + 1
+						y = y + 5
+						xn = xn - 4
+						yn = yn - 3
+						break;
+					case 6:
+						x = x - 1
+						y = y + 5
+						xn = xn - 2
+						yn = yn - 3
+						break;
+					case 8:
+						x = x - 4
+						y = y + 5
+						xn = xn + 1
+						yn = yn - 3
+						break;
+					case 10:
+						x = x - 5
+						y = y + 3
+						xn = xn + 2
+						yn = yn - 1
+						break;
+				}
+
+				let korr = j / 2 * 30 - 90;
+
+				if (korr < 0) korr = korr + 360;
+
+				canvas.text()
+					.text(korr)
+					.x(x)
+					.dy(y)
+					.addClass('mbText')
+
+				canvas.text()
+					.text((j / 2 * 30) + 90)
+					.x(xn)
+					.dy(yn)
+					.addClass('mbText')
+			}
 			canvas.path(trajectoryLine).addClass('mbCircle');
-			canvas.text('30').addClass('mbText');
-			
-			
 		};
-		
+
 		this.updateProperty = function (updatePropertyInfo) {
 			if (updatePropertyInfo.TargetProperty === 'CircleGraph Property') {
 				valueElem.text(updatePropertyInfo.SinglePropertyValue);
@@ -60,10 +114,11 @@
 			}
 			if (updatePropertyInfo.TargetProperty === 'CircleArray') {
 				let rows = updatePropertyInfo.ActualDataRows;
+
 				for (let i = 0; i < rows.length; i++) {
 					vibroData1[i] = Rad_Mill - rows[i].value;
 				}
-				
+
 				let alphaC = new Array();
 				let alphaS = new Array();
 
@@ -74,15 +129,15 @@
 
 				let trajectoryPoint = new SVG.PathArray();
 				trajectoryPoint[0] = ['M', alphaC[0], alphaS[0]];
+
 				for (let i = 1; i < points; i++) {
 					trajectoryPoint[i] = ['L', alphaC[i], alphaS[i]];
 				}
-				trajectoryPoint[points] = ['z'];
 
+				trajectoryPoint[points] = ['z'];
 				SVG.find('.trajectoryPath_A').remove();
 				canvas.path(trajectoryPoint).addClass('trajectoryPath_A');
-				
-				}
+			}
 		};
 	};
 }());

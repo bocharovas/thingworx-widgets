2bc6d4cda4237e252c83d1be1b7b7952df7ccb7e
Alexander Bocharov <alexander.bocharov@cadfem-cis.ru>
2022-04-15 16:52:02 +0300

    geburstagsdocuments

--- ui/circlegraph/circlegraph.runtime.js
+++ ui/circlegraph/circlegraph.runtime.js
@@ -126,10 +126,10 @@
 				case 'AngleCircleArray':
 					correctur = updatePropertyInfo.SinglePropertyValue;
 					break;
-				case 'Geschwindigkeit':
-					geschwindigkeit = updatePropertyInfo.SinglePropertyValue;
-					//ausbesserung = geschwindigkeit * koeffVerz10;
-					break;
+				/*case 'Geschwindigkeit':
+					//geschwindigkeit = updatePropertyInfo.SinglePropertyValue;
+					//ausbesserung = 10.0 * 1;
+					break;*/
 			}
 
 			if (updatePropertyInfo.TargetProperty === 'CircleGraph Property') {
@@ -141,11 +141,16 @@
 
 				let rows = updatePropertyInfo.ActualDataRows;
 
+				
+
 				for (let i = 0; i < rows.length; i++) {
 					rmsAccRawRel[i] = scale - rows[i].S603C01RMSAccRaw;
 					rmsAccRawAbs[i] = scale - rmsAccRawRel[i];
 					validity[i] = rows[i].S603C01Validity;
+					
 				}
+
+				geschwindigkeit = rows[1].xAxisPeakAcceleration;
 
 				let alphaC = new Array();
 				let alphaS = new Array();
@@ -155,7 +160,8 @@
 				//if (geschwindigkeit < 17.4) {koeffVerz10 = 1.49}
 				
 
-				console.log(geschwindigkeit);
+				
+				//console.log(ausbesserung);
 
 				//if (classC === 'trajectoryPath_B') {
 					
@@ -163,10 +169,60 @@
 					//ausbesserung = 0;
 				//}
 				
+				//ausbesserung = geschwindigkeit * 3.8;
+				//correctur = correctur + (geschwindigkeit * 3.8);
+				
+				
+				if (classC === 'trajectoryPath_B') {
+					if (geschwindigkeit < 13.7) {
+						ausbesserung = 30;
+					} else if (13.7 <= geschwindigkeit && geschwindigkeit < 17.4) {
+						ausbesserung = 40;
+					} else if (17.4 <= geschwindigkeit && geschwindigkeit < 19.5) {
+						ausbesserung = 70;
+					} else if (19.5 <= geschwindigkeit && geschwindigkeit < 21.6) {
+						ausbesserung = 95;
+					} else if (21.6 <= geschwindigkeit && geschwindigkeit < 23.7) {
+						ausbesserung = 110;
+					} else if (23.7 <= geschwindigkeit && geschwindigkeit < 25.8) {
+						ausbesserung = 115;
+					} else if (25.8 <= geschwindigkeit && geschwindigkeit < 28.9) {
+						ausbesserung = 130;
+					} else if (28.9 <= geschwindigkeit && geschwindigkeit < 32.0) {
+						ausbesserung = 140;
+					} else if (32.0 <= geschwindigkeit && geschwindigkeit < 35.1) {
+						ausbesserung = 150;
+					} else if (35.1 <= geschwindigkeit && geschwindigkeit < 38.3) {
+						ausbesserung = 160;
+					} else if (38.3 <= geschwindigkeit && geschwindigkeit < 42.5) {
+						ausbesserung = 170;
+					} else if (42.5 <= geschwindigkeit && geschwindigkeit < 46.7) {
+						ausbesserung = 180;
+					} else if (46.7 <= geschwindigkeit && geschwindigkeit < 50.9) {
+						ausbesserung = 190;
+					} else if (50.9 <= geschwindigkeit && geschwindigkeit < 54.8) {
+						ausbesserung = 200;
+					} else if (54.8 <= geschwindigkeit && geschwindigkeit < 59.3) {
+						ausbesserung = 210;
+					} else if (59.3 <= geschwindigkeit && geschwindigkeit < 70.0) {
+						ausbesserung = 220;
+					} else {
+						ausbesserung = 230;
+					}
 
+					console.log(geschwindigkeit);
+					console.log(ausbesserung);
+				}
+
+				//correctur += ausbesserung;
 
 				for (let i = 0; i < rows.length; i++) {
-					angle = 360 / rows.length * i - correctur;
+					if (classC === 'trajectoryPath_B') {
+						angle = 360 / rows.length * i - correctur - ausbesserung;
+					} else {
+						angle = 360 / rows.length * i - correctur;
+					}
+					
 					if (angle < 0) angle = angle + 360;
 					alphaC[i] = ((rmsAccRawRel[i] / scale) * Rad_Mill) *
 						Math.cos((angle) * (Math.PI / 180));
